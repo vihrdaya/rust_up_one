7d1cba91b87baf15aa05114f14ae694861c572cf
kevin <kevin.rajkumar@outlook.com>
2024-06-27 18:04:38 -0400

    minor add

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+    let random_number: i32 = rng.gen_range(1..10);
+    println!("Starting with random number: {}", random_number);
+    for current_number in 0..random_number {
+        if current_number % 2 == 0 {
+            println!("Hello, world!, number {}", current_number);
+        }
+    }
 }
